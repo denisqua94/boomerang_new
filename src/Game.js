a16624b05a181b030d2dc1697d5edab1e302a9a4
Denis <chepelnikov.denis@yandex.ru>
2021-12-17 16:44:01 +0300

    add class

--- src/Game.js
+++ src/Game.js
@@ -2,23 +2,27 @@
 // Или можно не импортировать,
 // а передавать все нужные объекты прямо из run.js при инициализации new Game().
 
-const Hero = require("./game-models/Hero");
-const Enemy = require("./game-models/Enemy");
-// const Boomerang = require('./game-models/Boomerang');
-const View = require("./View");
+const Hero = require('./game-models/Hero');
+const Enemy = require('./game-models/Enemy');
+const Boomerang = require('./game-models/Boomerang');
+const View = require('./View');
+const {keyboard} = require('./keyboard.js');
 
 // Основной класс игры.
 // Тут будут все настройки, проверки, запуск.
 
 class Game {
-    constructor({ trackLength }) {
-        this.trackLength = trackLength;
-        this.hero = new Hero(); // Герою можно аргументом передать бумеранг.
-        this.enemy = new Enemy();
-        this.view = new View();
-        this.track = [];
-        this.regenerateTrack();
-    }
+  constructor() {
+    this.trackLength = 150;
+    this.hero = new Hero(); // Герою можно аргументом передать бумеранг.
+    this.enemy = new Enemy();
+    this.view = new View();
+    this.boomerang = new Boomerang()
+    this.track = [];
+    this.regenerateTrack();
+   
+  }
+
 
     regenerateTrack() {
         // Сборка всего необходимого (герой, враг(и), оружие)
